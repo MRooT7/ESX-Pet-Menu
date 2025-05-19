10a20545689dcf627a039bc3e7861b0ecebe3b6b
MRooT7 <152092390+MRooT7@users.noreply.github.com>
2025-05-19 06:58:14 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ESX Pet System'
+author 'M.RooT'
+
+shared_script '@es_extended/imports.lua'
+
+shared_script 'shared/config.lua'
+
+client_script 'client/main.lua'
+
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
