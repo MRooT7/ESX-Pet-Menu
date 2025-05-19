41b59ed4fdf1cbac6ed73ad7d7a00203bd2ac722
MRooT7 <152092390+MRooT7@users.noreply.github.com>
2025-05-19 06:26:49 +0200

    Add files via upload

---
+++ items.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO items (name, label, weight, rare, can_remove) VALUES
+('cat_collar',      'Cat Collar',        1, 0, 1),
+('dog_collar',      'Dog Collar',        1, 0, 1),
+('rabbit_collar',   'Rabbit Collar',     1, 0, 1),
+('cow_collar',      'Cow Collar',        1, 0, 1),
+('deer_collar',     'Deer Collar',       1, 0, 1),
+('pig_collar',      'Pig Collar',        1, 0, 1),
+('lion_collar',     'Lion Collar',       1, 0, 1),
+('rat_collar',      'Rat Collar',        1, 0, 1),
+('chicken_collar',  'Chicken Collar',    1, 0, 1),
+('crow_collar',     'Crow Collar',       1, 0, 1),
+('pigeon_collar',   'Pigeon Collar',     1, 0, 1),
+('seagull_collar',  'Seagull Collar',    1, 0, 1),
+('monkey_collar',   'Monkey Collar',     1, 0, 1);
